e1491340dbfdaf792bbe47a29e4a18d77b80a5f5
Ahmed Wagih <ahmd.wagih96@gmail.com>
2022-10-07 14:17:25 +0200

    add background color to body

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useEffect } from "react";
 
-// Components 
+// Components
 import Login from "./Components/Login/Login";
 import WebcamCapture from "./Components/WebcamCapture/WebcamCapture";
 import Preview from "./Components/Preview/Preview";
@@ -9,30 +9,32 @@ import Chats from "./Components/Chats/Chats";
 import ChatView from "./Components/ChatView/ChatView";
 // React Roouter
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// Redux 
+// Redux
 import { useSelector, useDispatch } from "react-redux";
 import { login, selectedUser, logout } from "./features/appSlice";
 // Firebase
 import { auth } from "./firebase";
-import {onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
   const user = useSelector(selectedUser);
   const dispatch = useDispatch();
-  
-  useEffect(()=>{
-    onAuthStateChanged(auth, (user)=>{
-      if(user){
-        dispatch(login({
-          username: user.displayName,
-          profilePic: user.photoURL,
-          id: user.uid,
-        }))
-      }else{
-        dispatch(logout())
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(
+          login({
+            username: user.displayName,
+            profilePic: user.photoURL,
+            id: user.uid,
+          })
+        );
+      } else {
+        dispatch(logout());
       }
-    })
-  },[])
+    });
+  }, []);
 
   return (
     <div className="app">
@@ -40,12 +42,14 @@ function App() {
         {!user ? (
           <Login />
         ) : (
-          <Routes>
-            <Route path="/" element={<WebcamCapture />} />
-            <Route path="/preview" element={<Preview />} />
-            <Route path="/chats" element={<Chats />} />
-            <Route path="/chats/view" element={<ChatView />} />
-          </Routes>
+          <div className="app__bodyBackground">
+            <Routes>
+              <Route path="/" element={<WebcamCapture />} />
+              <Route path="/preview" element={<Preview />} />
+              <Route path="/chats" element={<Chats />} />
+              <Route path="/chats/view" element={<ChatView />} />
+            </Routes>
+          </div>
         )}
       </BrowserRouter>
     </div>
