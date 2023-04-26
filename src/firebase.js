debd7c9efbb774b645bf2a3154de0ba26de55f93
Ahmed Wagih <ahmd.wagih96@gmail.com>
2023-04-27 01:01:01 +0200

    added local variables

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBYwsZHv7PBGJKNjRnBNeP0uc2lcY6SUMo",
-  authDomain: "clone-3eeb3.firebaseapp.com",
-  projectId: "clone-3eeb3",
-  storageBucket: "clone-3eeb3.appspot.com",
-  messagingSenderId: "913180479576",
-  appId: "1:913180479576:web:d164ecd9e5514b5fb93a2d",
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`,
 };
 
 const app = initializeApp(firebaseConfig);
