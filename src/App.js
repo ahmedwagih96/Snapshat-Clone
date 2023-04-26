84ab817d948b63ab7e9810eee46b68c2a093c337
Ahmed Wagih <ahmd.wagih96@gmail.com>
2023-04-27 00:55:41 +0200

    fixed the redirection pug to login page

--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
+import { useEffect } from "react";
 import "./App.css";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-// Components
-import Login from "./Components/Login";
+// React Toast
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+// Pages
+import Login from "./pages/Login";
 import WebcamCapture from "./Components/WebcamCapture";
 import Preview from "./Components/Preview";
 import Chats from "./Components/Chats";
 import ChatView from "./Components/ChatView/ChatView";
-
 // React Router
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import useAuthState from "./hooks/useAuthState";
-
+import { Routes, Route, useNavigate } from "react-router-dom";
+// Firebase
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
+// Custom Hooks
+import useUserSlice from "./hooks/useUserSlice";
 function App() {
-  const user = useAuthState();
+  const { loginToApp, logoutFromApp } = useUserSlice();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        loginToApp({
+          username: user.displayName,
+          profilePic: user.photoURL,
+          id: user.uid,
+        });
+        navigate("/");
+      } else {
+        logoutFromApp();
+        navigate("/login");
+      }
+    });
+  }, [loginToApp, logoutFromApp, navigate]);
+
   return (
     <div className="app">
-      <BrowserRouter>
-        {!user ? (
-          <Login />
-        ) : (
-          <div className="app__bodyBackground">
-            <Routes>
-              <Route path="/" element={<WebcamCapture />} />
-              <Route path="/preview" element={<Preview />} />
-              <Route path="/chats" element={<Chats />} />
-              <Route path="/chats/view" element={<ChatView />} />
-            </Routes>
-          </div>
-        )}
-      </BrowserRouter>
-      <ToastContainer
-       />
-
+      <div className="app__bodyBackground">
+        <Routes>
+          <Route path="/" element={<WebcamCapture />} />
+          <Route path="/preview" element={<Preview />} />
+          <Route path="/chats" element={<Chats />} />
+          <Route path="/chats/view" element={<ChatView />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+      <ToastContainer />
     </div>
   );
 }
